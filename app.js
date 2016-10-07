350e8b222c24e66c8b23209a03b66dd395d334b5
simplon <rudy__10@hotmail.fr>
2016-10-07 16:35:57 +0200

    ajout fichier js index

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
+
 $(document).ready(function(){
-	//votre code
+	var i=0;
+
+	var tab =["change de jeux", "Je t'ai dit de changer de jeux", "Tu m'enerves", "arret de jouer","Ciao"];
+
+	$(".cake-is-a-lie").click(function(){
+		i++;
+		$(".counter").text(i);
+
+		if (i%20===0)
+		{
+			
+			var textaleatoire = Math.floor(Math.random()*(5-1))+1;
+			var aleatoire = tab[textaleatoire]
 
 
-});
+			alert(aleatoire);
+
+		}
+
+	});
+
+})
+
+
+
